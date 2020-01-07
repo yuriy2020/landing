2c14eb6fd5daef0e3527d56122111e9fc32f7dbe
Yuri Balalaev <yuriy8888@yandex.ru>
2020-01-07 15:24:49 +0300

    slider is wworking

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 $(function () {
 
-    $('.slider__inner').slick();
+    $('.slider__inner').slick({
+        nextArrow: '<button type="button" class="slick__btn slick-next"></button>',
+        prevArrow: '<button type="button" class="slick__btn slick-prev"></button>'
+    });
 
 
 
